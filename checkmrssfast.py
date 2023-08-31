427f3faa016e9031f5a3da3437dcee12a87d6133
Jason Perr <trezero@gmail.com>
2023-08-31 14:45:00 -0700

    working better

--- checkmrssfast.py
+++ checkmrssfast.py
@@ -1,46 +1,44 @@
 import requests
-import xml.etree.ElementTree as ET
+from xml.etree import ElementTree as ET
 import sys
 from concurrent.futures import ThreadPoolExecutor
 
-def check_url(url, entry_id):
-    if url == 'NOT_POSSIBLE_TO_GENERATE_URL_FOR_FILE':
-        print(f'Key_Art file for ID {entry_id} was not found')
-        return True
+def check_url(url):
+    if not url or "://" not in url:
+        print(f"Skipping invalid URL: {url}")
+        return True  # or False, depending on how you want to handle this case
+
     try:
         response = requests.head(url, allow_redirects=True)
         if response.headers.get('Content-Type') == 'application/xml':
             response = requests.get(url)
-            if '<Code>AccessDenied</Code>' in response.text:
+            if "<Code>AccessDenied</Code>" in response.text:
                 return False
     except Exception as e:
-        print(f'Error checking URL {url}: {e}')
+        print(f"Error checking URL {url}: {e}")
     return True
 
-def check_urls(urls, entry_ids):
+def check_urls(urls):
     with ThreadPoolExecutor() as executor:
-        results = list(executor.map(check_url, urls, entry_ids))
+        results = list(executor.map(check_url, urls))
     return results
 
 def main(feed_url):
     response = requests.get(feed_url)
     root = ET.fromstring(response.content)
-    urls_to_check = []
-    entry_ids = []
-    for entry in root.findall('.//item'):
-        entry_id = entry.find('id').text if entry.find('id') is not None else 'Unknown'
-        urls = [elem.attrib['url'] for elem in entry.findall('.//media:thumbnail', namespaces={'media': 'http://search.yahoo.com/mrss/'})]
-        urls_to_check.extend(urls)
-        entry_ids.extend([entry_id] * len(urls))
-    results = check_urls(urls_to_check, entry_ids)
+
+    urls_to_check = [element.get('url') for element in root.findall(".//*[@url]")]
+    results = check_urls(urls_to_check)
     non_downloadable_urls = [url for url, result in zip(urls_to_check, results) if not result]
-    print('URLs with Access Denied:')
+
+    print("URLs with Access Denied:")
     for url in non_downloadable_urls:
         print(url)
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     if len(sys.argv) != 2:
-        print('Usage: python checkmrss.py <MRSS_Feed_URL>')
+        print("Usage: python checkmrss.py <MRSS_Feed_URL>")
         sys.exit(1)
+
     feed_url = sys.argv[1]
     main(feed_url)
--- checkmrssfast_OLD.py
+++ checkmrssfast_OLD.py
@@ -1,28 +1,44 @@
 import requests
-import xml.etree.ElementTree as ET
+from xml.etree import ElementTree as ET
+import sys
 from concurrent.futures import ThreadPoolExecutor
 
 def check_url(url):
-    if not url or '://' not in url:
-        print(f'Invalid URL: {url}')
-        return
+    if not url or "://" not in url:
+        print(f"Skipping invalid URL: {url}")
+        return True  # or False, depending on how you want to handle this case
+
     try:
         response = requests.head(url, allow_redirects=True)
         if response.headers.get('Content-Type') == 'application/xml':
             response = requests.get(url)
-            if '<Code>AccessDenied</Code>' in response.text:
-                print(f'Access Denied: {url}')
+            if "<Code>AccessDenied</Code>" in response.text:
+                return False
     except Exception as e:
-        print(f'Error checking URL {url}: {e}')
+        print(f"Error checking URL {url}: {e}")
+    return True
 
-def main(mrss_feed_url):
-    response = requests.get(mrss_feed_url)
-    root = ET.fromstring(response.content)
-    urls = [elem.attrib['url'] for elem in root.findall('.//media:thumbnail', namespaces={'media': 'http://search.yahoo.com/mrss/'})]
-
+def check_urls(urls):
     with ThreadPoolExecutor() as executor:
-        executor.map(check_url, urls)
+        results = list(executor.map(check_url, urls))
+    return results
 
-if __name__ == '__main__':
-    mrss_feed_url = 'https://s3.us-west-2.amazonaws.com/iconik-mrss-feeds/mrss-collection-63c43720-0581-11ee-9dea-dee45977c716.xml'
-    main(mrss_feed_url)
+def main(feed_url):
+    response = requests.get(feed_url)
+    root = ET.fromstring(response.content)
+
+    urls_to_check = [element.get('url') for element in root.findall(".//*[@url]")]
+    results = check_urls(urls_to_check)
+    non_downloadable_urls = [url for url, result in zip(urls_to_check, results) if not result]
+
+    print("URLs with Access Denied:")
+    for url in non_downloadable_urls:
+        print(url)
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: python checkmrss.py <MRSS_Feed_URL>")
+        sys.exit(1)
+
+    feed_url = sys.argv[1]
+    main(feed_url)
